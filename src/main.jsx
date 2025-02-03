e8a0f29f0b2164e9cb9da7289be8fa65216e2399
Vipin Kumar Singh <109547853+vipin9575@users.noreply.github.com>
2025-02-03 19:46:57 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/mindgrow">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
