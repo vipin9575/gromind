0b9bf832610519180d0a886feb19d32613b7f079
Vipin Kumar Singh <109547853+vipin9575@users.noreply.github.com>
2025-02-19 22:23:39 +0530

    dynamic title and meta

---
+++ App.jsx
@@ -0,0 +1,50 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "../src/components/home/Home";
+import Footer from "./components/footer/Footer";
+import AboutUsPage from "./pages/AboutUsPage";
+import CouresPage from "./pages/CouresPage";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import "./App.css";
+import ContactUsPage from "./pages/ContactUsPage";
+import EventsPage from "./pages/EventsPage";
+import ScrollToTop from "./ScrollToTop";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
+import TermsOfServicePage from "./pages/TermsOfServicePage";
+import { ToastContainer } from "react-toastify";
+import BlogsPage from "./pages/BlogsPage";
+import { HelmetProvider } from "react-helmet-async";
+
+const App = () => {
+  return (
+    <>
+      <HelmetProvider>
+        <ToastContainer />
+        <ThemeProvider theme={theme}>
+          <ScrollToTop />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUsPage />} />
+            <Route path="/courses" element={<CouresPage />}>
+              <Route path="/courses/:id" element={<CouresPage />} />
+            </Route>
+
+            <Route path="/contact-us" element={<ContactUsPage />} />
+            <Route path="/events" element={<EventsPage />}>
+              <Route path="/events/:event_name/:id" element={<EventsPage />} />
+            </Route>
+
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            <Route path="/terms-of-service" element={<TermsOfServicePage />} />
+            <Route path="/blogs" element={<BlogsPage />} />
+          </Routes>
+          <Footer />
+        </ThemeProvider>
+      </HelmetProvider>
+    </>
+  );
+};
+
+export default App;
